5c47f29a3cc2c80afba25976150dbd5ae26bd926
INBGNL0350\Pooja CK <pooja.ck@asmltd.com>
2022-12-15 17:23:36 +0530

    music

--- hungry.py
+++ hungry.py
@@ -1,13 +1,18 @@
-a = input("im hungery?")
+a = input("ru hungery?")
 if a == "yes":
     print("eat something")
-    print(" u can eat pizza")
-    print(" or eat burger")
+    print("u can eat pizza")
+    print("or eat burger")
 else:
     print("go sleep")
-b= input("do u want to learn git?")
+b = input("do u want to learn git?")
 if b == "yes":
     print("good")
 else:
     print("then ur waste")
+c = input("u like music?")
+if c =="yes":
+    print("good habit!")
+else:
+    print("start to listen")
 
