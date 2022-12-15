c36d5ff707e811898ccf07ba5d99c4ebd8d915e3
INBGNL0350\Pooja CK <pooja.ck@asmltd.com>
2022-12-15 14:52:09 +0530

    3rd commit

--- hungry.py
+++ hungry.py
@@ -2,5 +2,10 @@ a = input("im hungery?")
 if a == "yes":
     print("eat something")
 else:
-    print("go aleep")
+    print("go sleep")
+b= input("do u want to learn git?")
+if b == "yes":
+    print("good")
+else:
+    print("then ur waste")
 
